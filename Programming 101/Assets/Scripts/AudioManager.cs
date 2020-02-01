8b7441a6d50fd493532b55274bbfe236b1cf84f2
Marc <magueracoras@gmail.com>
2020-02-01 23:51:52 +0100

    Audio y cosas

---
+++ Programming 101/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip[] clips;
+
+    public void PlayAudio(int index, float volume, float pitch)
+    {
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+        source.clip = clips[index];
+        source.volume = volume;
+        source.pitch = pitch;
+
+        source.Play();
+
+        Destroy(source, clips[index].length);
+    }
+}
